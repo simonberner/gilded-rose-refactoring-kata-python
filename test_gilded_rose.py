8e4e4e5f9eb8bb8154e0af24c54797cec37ed9cb
Simon Berner <sibebzh@gmail.com>
2024-05-22 18:44:16 +0200

    add: :sparkles: set the starting position
    Add the Gilded Rose starting position in Python

---
+++ test_gilded_rose.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from gilded_rose import Item, GildedRose
+
+
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert "fixme" == items[0].name
