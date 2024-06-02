f825ae45d6ecac7bb4e46d2e386b32a52886cab6
Simon Berner <sibebzh@gmail.com>
2024-06-02 18:49:03 +0200

    refactor: :recycle: extract special items to class constants
    The code was refactored to replace the hard-coded item names with class variables. This improves readability, maintainability, and allows for easy updates in case the item names change in the future. The changes were made in the conditions that check item names during quality and selling day adjustments.

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,6 +11,10 @@ class GildedRose(object):
 
 
 class Item:
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -26,9 +30,9 @@ class Item:
     def __calc_quality_when_selling_date_reached(self):
         # Once the sell by date has passed, Quality degrades twice as fast (meaning an additional decrease of -1)
         if self.sell_in < 0:
-            if self.name != "Aged Brie":
-                if self.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if self.name != "Sulfuras, Hand of Ragnaros" and self.quality > 0:
+            if self.name != Item.AGED_BRIE:
+                if self.name != Item.BACKSTAGE_PASS:
+                    if self.name != Item.SULFURAS and self.quality > 0:
                         # Decrease quality
                         self.quality = self.quality - 1
                 # Quality drops to 0 after the concert for Backstage passes
@@ -41,14 +45,14 @@ class Item:
 
     def __adjust_quality(self):
         # Decrease the quality of normal item
-        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert" and self.name != "Sulfuras, Hand of Ragnaros":
+        if self.name != Item.AGED_BRIE and self.name != Item.BACKSTAGE_PASS and self.name != Item.SULFURAS:
             if self.quality > 0:
                 self.quality = self.quality - 1
         # Increase quality for special items
         else:
             if self.quality < 50:
                 self.quality = self.quality + 1
-                if self.name == "Backstage passes to a TAFKAL80ETC concert":
+                if self.name == Item.BACKSTAGE_PASS:
                     if self.sell_in < 11:
                         if self.quality < 50:
                             self.quality = self.quality + 1
@@ -57,7 +61,7 @@ class Item:
                             self.quality = self.quality + 1
 
     def __decrease_day(self):
-        if self.name != "Sulfuras, Hand of Ragnaros":
+        if self.name != Item.SULFURAS:
             self.sell_in = self.sell_in - 1
 
     def __repr__(self):
