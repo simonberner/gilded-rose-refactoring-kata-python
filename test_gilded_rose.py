c3d58f2ca94adb7bfc3b5eaca7ab13fc7c09d61f
Simon Berner <sibebzh@gmail.com>
2024-06-21 16:25:09 +0200

    doc: :memo: add some test ideas
    Add some test ideas to get a feeling of how many relevant test combination there could be.

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -8,3 +8,29 @@ def test_normal_item_quality_degrades_twice_as_fast():
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert 2 == items[0].quality
+
+# More Test Ideas to cover the business rules:
+#
+# Normal item: at the end of each day the Quality drops by 1
+#
+# All items: at the end of each day the SellIn drops by 1 (except legendary item Sulfuras)
+# All items: The Quality of an item is never negative
+# All items: The Quality of an item is never more than 50
+#
+# Special items:
+# "Aged Brie" Quality increases by 1 as long SellIn > 0 days
+# "Aged Brie" Quality is never < 0
+# "Aged Brie" never decreases in Quality
+# "Backstage pass" Quality is never < 0
+# "Backstage pass" never decreases in Quality
+# "Backstage pass" Quality increases by 1 when SellIn > 10 days
+# "Backstage pass" Quality increases by 2 when SellIn <= 10 days
+# "Backstage pass" Quality increases by 3 when SellIn <= 5 days
+# "Backstage pass" Quality drops to 0 when SellIn == 0 day
+#
+# Legendary item: "Sulfuras" has a Quality of 80 and never decreases.
+# Legendary item: "Sulfuras" SellIn does not change because it never has to be sold
+
+# Questions
+# Are there any other "Special Items" in the shop or will be there in the future with some other specific rules to consider??
+# Is "Sulfuras" currently the only "Legendary Item" in the shop or will be there more in the future?
