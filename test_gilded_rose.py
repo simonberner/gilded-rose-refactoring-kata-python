613e384548bb8197008e1a24185e4a9a6d16bdd1
Simon Berner <sibebzh@gmail.com>
2024-06-21 16:21:51 +0200

    test: :white_check_mark: make first test green

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -2,8 +2,9 @@
 from gilded_rose import Item, GildedRose
 
 
-def test_foo():
-    items = [Item("foo", 0, 0)]
+#  Normal item: once the SellIn == 0, Quality degrades twice as fast
+def test_normal_item_quality_degrades_twice_as_fast():
+    items = [Item("foo", 0, 4)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert "fixme" == items[0].name
+    assert 2 == items[0].quality
