1b38f2cbab5906aaa9de95cfdc204209fcabdcde
Simon Berner <sibebzh@gmail.com>
2024-07-15 17:01:14 +0200

    fix: :bug: fix var naming

--- gilded_rose.py
+++ gilded_rose.py
@@ -17,53 +17,51 @@ class Item:
         self.quality = quality
 
     def update_item(self):
-        # If not Aged Brie and not Backstage passes
-        is_aged_brie = self.name != "Aged Brie"
-        is_backstage_pass = self.name != "Backstage passes to a TAFKAL80ETC concert"
-        is_sulfuras = self.name != "Sulfuras, Hand of Ragnaros"
+        is_not_aged_brie = self.name != "Aged Brie"
+        is_not_backstage_pass = self.name != "Backstage passes to a TAFKAL80ETC concert"
+        is_not_sulfuras = self.name != "Sulfuras, Hand of Ragnaros"
 
-        if is_aged_brie:
-            # ifelse
-            if is_backstage_pass:
+        if is_not_aged_brie:
+            if is_not_backstage_pass:
                 if self.quality > 0:
-                    if is_sulfuras:
+                    if is_not_sulfuras:
                         self.quality = self.quality - 1
             else:
                 if self.quality < 50:
                     self.quality = self.quality + 1
-                    if not is_backstage_pass:
+                    if not is_not_backstage_pass:
                         if self.sell_in < 11 and self.quality < 50:
                             self.quality = self.quality + 1
                         if self.sell_in < 6 and self.quality < 50:
                             self.quality = self.quality + 1
             # Updating sellIn when not Sulfuras
-            if is_sulfuras:
+            if is_not_sulfuras:
                 self.sell_in = self.sell_in - 1
             # Code smell: repeated conditional
             if self.sell_in < 0:
                 # ifelse
-                if is_backstage_pass:
+                if is_not_backstage_pass:
                     if self.quality > 0:
-                        if is_sulfuras:
+                        if is_not_sulfuras:
                             self.quality = self.quality - 1
                 else:
                     self.quality -= self.quality
         else:
             # ifelse
-            if not is_backstage_pass:
+            if not is_not_backstage_pass:
                 if self.quality > 0:
-                    if is_sulfuras:
+                    if is_not_sulfuras:
                         self.quality = self.quality - 1
             else:
                 if self.quality < 50:
                     self.quality = self.quality + 1
-                    if not is_backstage_pass:
+                    if not is_not_backstage_pass:
                         if self.sell_in < 11 and self.quality < 50:
                             self.quality = self.quality + 1
                         if self.sell_in < 6 and self.quality < 50:
                             self.quality = self.quality + 1
             # Updating sellIn when not Sulfuras
-            if is_sulfuras:
+            if is_not_sulfuras:
                 self.sell_in = self.sell_in - 1
             # Code smell: repeated conditional
             if self.sell_in < 0 and self.quality < 50:
